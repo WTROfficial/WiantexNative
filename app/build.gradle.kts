452d4f4e0b99b2d72d97814303a8507d6f821968
WTROfficial <wtrthegamer@gmail.com>
2026-09-03 01:40:30 +0300

    Fix Kotlin and Java JVM target

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,8 +3,32 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-android { namespace = "com.wiantex.client"; compileSdk = 35
-    defaultConfig { applicationId = "com.wiantex.client"; minSdk = 24; targetSdk = 35; versionCode = 1; versionName = "1.0" }
+android {
+    namespace = "com.wiantex.client"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.wiantex.client"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
